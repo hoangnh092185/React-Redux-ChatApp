c0707317a3699044f1f706e99c4789dcc54e50d7
Nhat Hoang <hoangnh092185@gmail.com>
2017-04-12 10:24:33 -0700

    Turn App into a stateless component.

--- public/app.js
+++ public/app.js
@@ -93,34 +93,12 @@ function messagesReducer(state = [], action) {
 
 const store = Redux.createStore(reducer);
 
-
-class App extends React.Component {
-  componentDidMount() {
-    store.subscribe(() => this.forceUpdate());
-  }
-
-  render() {
-    const state = store.getState();
-    const activeThreadId = state.activeThreadId;
-    const threads = state.threads;
-    const activeThread = threads.find((t) => t.id === activeThreadId);
-
-    const tabs = threads.map(t => (
-      {
-        title: t.title,
-        active: t.id === activeThreadId,
-        id: t.id,
-      }
-    ));
-
-    return (
-      <div className='ui segment'>
-        <ThreadTabs tabs={tabs} />
-        <Thread thread={activeThread} />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className='ui segment'>
+    <ThreadTabs />
+    <ThreadDisplay />
+  </div>
+);
 
 /* eslint-disable react/prefer-stateless-function */
 const Tabs = (props) => (
